f46d8ed65cf51e81393b76f543497e2023a3ad5c
novipro88 <noviantipali97@gmail.com>
2024-05-16 09:12:02 +0700

    Video Audio Call Channel

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,43 @@
-import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server'
+import { authMiddleware, redirectToSignIn } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
-const isPublicRoute = createRouteMatcher(['/sign-in', '/sign-up']);
+// See https://clerk.com/docs/references/nextjs/auth-middleware
+// for more information about configuring your Middleware
 
-export default clerkMiddleware((auth, request) => {
-  if (!isPublicRoute(request)) {
-    auth().protect();
-  }
+export default authMiddleware({
+  // Allow signed out users to access the specified routes:
+  // publicRoutes: ['/anyone-can-visit-this-route'],
+  // Prevent the specified routes from accessing
+  // authentication information:
+  // ignoredRoutes: ['/no-auth-in-this-route'],
+
+  async afterAuth(auth, request) {
+    // if users are not authenticated
+    if (!auth.userId && !auth.isPublicRoute) {
+      return redirectToSignIn({ returnBackUrl: request.url });
+    }
+
+    // If user has signed up, register on Stream backend
+    if (auth.userId && !auth.user?.privateMetadata?.streamRegistered) {
+      // return redirect('/register');
+    } else {
+      console.log(
+        '[Middleware] User already registered on Stream backend: ',
+        auth.userId
+      );
+    }
+
+    return NextResponse.next();
+  },
 });
 
 export const config = {
-  matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
-}
+  matcher: [
+    // Exclude files with a "." followed by an extension, which are typically static files.
+    // Exclude files in the _next directory, which are Next.js internals.
+
+    '/((?!.+\\.[\\w]+$|_next).*)',
+    // Re-include any files in the api or trpc folders that might have an extension
+    '/(api|trpc)(.*)',
+  ],
+};
